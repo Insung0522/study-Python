b1717cb3126d05a50a83d73faa50cf0ab7dca674
ispark96 <pkani1027@gmail.com>
2024-08-06 14:45:39 +0900

    이미지 확대 및 축소

--- Study_Py_OpenCV/Study_Py_OpenCV_03.py
+++ Study_Py_OpenCV/Study_Py_OpenCV_03.py
@@ -20,11 +20,22 @@ height, width, channel = src.shape # 해당 이미지의 높이, 넓이, 채널 
 # getRotationMatrix2D(center, angle, scale) center = 중심점, angle = 각도, scale = 비율로 매핑 변환 행렬 생성
 # 중심점은 튜플 형태로 기준점 설정
 matrix = cv2.getRotationMatrix2D((width/2, height/2), 90, 1) 
-print(matrix)
 # warpAffine(원본 이미지, 아핀 맵 행렬, 출력 이미지 크기)
 dst = cv2.warpAffine(src, matrix, (width, height))
 
 cv2.imshow("src", src)
 cv2.imshow("dst", dst)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+# 확대 & 축소
+src = cv2.imread("Study_Py_OpenCV/fruits.jpg", cv2.IMREAD_COLOR)
+height, width, cahnnel = src.shape
+dst = cv2.pyrUp(src, dstsize=(width * 2, height * 2), borderType=cv2.BORDER_DEFAULT)
+dst2 = cv2.pyrDown(src)
+
+cv2.imshow("src",src)
+cv2.imshow("dst",dst)
+cv2.imshow("dst2",dst)
 cv2.waitKey()
 cv2.destroyAllWindows()
