2c59e679430a11de96a16b9c972f720211619da6
ispark96 <pkani1027@gmail.com>
2024-08-08 10:12:57 +0900

    이미지, 동영상 오픈 및 출력

--- Study_Py_OpenCV/Study_Py_OpenCV_02.py
+++ Study_Py_OpenCV/Study_Py_OpenCV_02.py
@@ -1,13 +1,13 @@
 import cv2
 
 #이미지 출력
-image = cv2.imread("Study_Py_OpenCV/lunar.jpg", cv2.IMREAD_ANYCOLOR)
+image = cv2.imread("Study_Py_OpenCV/src/lunar.jpg", cv2.IMREAD_ANYCOLOR)
 cv2.imshow("Moon", image)
 cv2.waitKey()
 cv2.destroyAllWindows()
 
 #비디오 출력
-capture = cv2.VideoCapture("Study_Py_OpenCV/sample.mp4")
+capture = cv2.VideoCapture("Study_Py_OpenCV/src/sample.mp4")
 capture.isOpened() # 파일 읽기 성공 여부 확인
 
 while cv2.waitKey(33) < 0:
